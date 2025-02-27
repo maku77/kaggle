818ea1aaf4b8806b26f10f405fe3073ebc8731d6
maku77 <makkuma77@gmail.com>
2025-02-28 01:39:58 +0900

    Refine pipeline code

--- playground-series-s5e2/main.py
+++ playground-series-s5e2/main.py
@@ -1,21 +1,26 @@
+import sys
 from abc import abstractmethod
 
 import lightgbm
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import cross_validate
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import OneHotEncoder, PolynomialFeatures
 
 
 class BaseModel:
-    def __init__(self, *, extra=True, small_test=False) -> None:
+    def __init__(
+        self, *, extra=True, small_test=False, outfile="submission.csv"
+    ) -> None:
         """
         学習用データを読み込みます。
         - extra: 追加の学習用データをマージする場合は True
         - small_test: 全体の流れを確認するために少量のデータで動かす場合 True
         """
+        # train = pd.read_csv("data/training_extra.csv", index_col="id")
         train = pd.read_csv("data/train.csv", index_col="id")
         if extra:
             ext = pd.read_csv("data/training_extra.csv", index_col="id")
@@ -27,6 +32,7 @@ class BaseModel:
 
         self.x_train = train.drop("Price", axis=1)
         self.y_train = train["Price"]
+        self.outfile = outfile
 
     def validate(self, fold=5) -> None:
         """
@@ -42,10 +48,12 @@ class BaseModel:
             scoring="neg_mean_squared_error",
             n_jobs=-1,
         )
-        rmse = (metrics["test_score"].mean() * -1) ** 0.5
-        print(f"RMSE: {rmse.round(3)}\n")  # Root Mean Squared Error
 
-    def fit_and_predict(self, outfile="submission.csv") -> None:
+        # Print Root Mean Squared Error
+        rmse = (metrics["test_score"].mean() * -1) ** 0.5
+        print(f"RMSE: {rmse.round(3)}\n")
+
+    def fit_and_predict(self) -> None:
         """
         モデルの学習とテストデータの予測を行い、提出用のファイルを作成します。
         """
@@ -59,14 +67,16 @@ class BaseModel:
         pred = pipeline.predict(test)
 
         pred_df = pd.DataFrame({"id": test.index, "Price": pred.round(3)})
-        pred_df.to_csv(outfile, index=False)
-        print(f"Submission file is created: {outfile}\n")
+        pred_df.to_csv(self.outfile, index=False)
+        print(f"Submission file is created: {self.outfile}\n")
 
-    def _get_categorical_columns(self):
-        """
-        カテゴリ変数のカラム一覧を取得します。
-        """
+    def get_cat_cols(self):
+        """Get the list of categorical columns."""
         return self.x_train.select_dtypes(include=["object"]).columns.to_list()
+
+    def get_num_cols(self):
+        """Get the list of numerical columns."""
+        return self.x_train.select_dtypes(include=["number"]).columns.to_list()
 
     @abstractmethod
     def _create_pipeline(self) -> Pipeline:
@@ -78,57 +88,62 @@ class BaseModel:
 
 class RandomForestModel(BaseModel):
     def __init__(self, *, small_test=False) -> None:
-        super().__init__(small_test=small_test)
+        super().__init__(small_test=small_test, outfile="submission-random-forest.csv")
 
     def _create_pipeline(self) -> Pipeline:
-        # 各カラムの変換器を作成（カテゴリ変数は One-hot エーコーディング、それ以外はそのまま残す）
-        preprocessor = ColumnTransformer(
-            transformers=[
-                (
-                    "OH",
-                    OneHotEncoder(sparse_output=False, handle_unknown="ignore"),
-                    self._get_categorical_columns(),
-                )
-            ],
+        # カテゴリ変数用の変換器
+        cat_transformer = Pipeline(
+            steps=[
+                ("OneHot", OneHotEncoder(sparse_output=False, handle_unknown="ignore")),
+            ]
+        )
+        # カラムの種類に応じて変換器を割り当て
+        transformer = ColumnTransformer(
+            transformers=[("categorical", cat_transformer, self.get_cat_cols())],
             remainder="passthrough",
         )
-        return Pipeline(
-            steps=[
-                ("preprocessor", preprocessor),
-                ("model", RandomForestRegressor(random_state=0, n_jobs=-1)),
-            ],
-            verbose=False,
-        )
+        # 予測モデル
+        estimator = RandomForestRegressor(random_state=0, n_jobs=-1)
+        return make_pipeline(transformer, estimator)
 
 
 class LgbmModel(BaseModel):
     def __init__(self, *, small_test=False) -> None:
-        super().__init__(small_test=small_test)
+        super().__init__(small_test=small_test, outfile="submission-lgbm.csv")
 
     def _create_pipeline(self) -> Pipeline:
-        # 各カラムの変換器を作成（カテゴリ変数は One-hot エーコーディング、それ以外はそのまま残す）
-        preprocessor = ColumnTransformer(
+        # カテゴリ変数用の変換器
+        cat_transformer = Pipeline(
+            steps=[
+                ("OneHot", OneHotEncoder(sparse_output=False, handle_unknown="ignore")),
+            ]
+        )
+        # 数値変数用の変換器
+        num_transformer = Pipeline(
+            steps=[
+                ("SimpleImputer", SimpleImputer(strategy="mean")),
+                ("Polynomial", PolynomialFeatures(degree=2, include_bias=False)),
+            ]
+        )
+        # カラムの種類に応じて変換器を割り当て
+        transformer = ColumnTransformer(
             transformers=[
-                (
-                    "OH",
-                    OneHotEncoder(sparse_output=False, handle_unknown="ignore"),
-                    self._get_categorical_columns(),
-                )
+                ("categorical", cat_transformer, self.get_cat_cols()),
+                ("numerical", num_transformer, self.get_num_cols()),
             ],
             remainder="passthrough",
         )
-        model = lightgbm.LGBMRegressor(random_state=0, force_col_wise=True, n_jobs=-1)
-        return Pipeline(
-            steps=[("preprocessor", preprocessor), ("model", model)],
-            verbose=False,
+        # 予測モデル
+        estimator = lightgbm.LGBMRegressor(
+            random_state=0, force_col_wise=True, n_jobs=-1
         )
+        return make_pipeline(transformer, estimator)
 
 
 if __name__ == "__main__":
-    m1 = LgbmModel(small_test=False)
-    m1.fit_and_predict("submission-lgbm.csv")
-    # m1.validate()
-
-    # m2 = RandomForestModel(small_test=False)
-    # m2.fit_and_predict("submission-random-forest.csv")
-    # m2.validate()
+    # m = RandomForestModel(small_test=False)
+    m = LgbmModel(small_test=False)
+    if len(sys.argv) > 1 and sys.argv[1].startswith("--val"):
+        m.validate()
+    else:
+        m.fit_and_predict()
